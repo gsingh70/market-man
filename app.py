13b8485daaba43426b72c5bdcb5b769749b32628
Gurjot Singh <singh95gurjot@gmail.com>
2022-01-13 19:01:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 @app.route("/", methods=['post', 'get'])
 def index():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get(GOOGLE_CHROME_BIN)
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
     driver.get('https://www.nseindia.com/get-quotes/equity?symbol=BCG')
 
     # Wait for the page to fully load
