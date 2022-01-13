d35b26b89a351d22c9f25eb8d4e6ecee370a852a
Gurjot Singh <singh95gurjot@gmail.com>
2022-01-13 14:09:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,24 +3,20 @@ import psycopg2
 import pandas as pd
 from selenium import webdriver
 from bs4 import BeautifulSoup
-from selenium.webdriver.chrome.options import Options
 import time
 import os
+from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['post', 'get'])
 def index():
-    options = Options()
-
-    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
-
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--remote-debugging-port=9222')
-    driver=webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
+    chrome_options = Options()
+    chrome_options.binary_location = "GOOGLE_CHROME_BIN"
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path="CHROMEDRIVER_PATH", chrome_options=chrome_options)
     driver.get('https://www.nseindia.com/get-quotes/equity?symbol=BCG')
 
     # Wait for the page to fully load
